34b7c1890fd61ffb5525da08688e28823be43237
kaluram kharra <75246076+kaluramkharra@users.noreply.github.com>
2023-08-21 20:16:00 +0530

    sab lut gya

--- backend/settings.py
+++ backend/settings.py
@@ -157,10 +157,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000','https://tensorcodes.com',"*"
+    'http://localhost:3000','https://tensorcodes.com',"https://kisanbazaar-kaluramkharra.vercel.app/"
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    'http://localhost:3000','https://tensorcodes.com',"*"
+    'http://localhost:3000','https://tensorcodes.com',"https://kisanbazaar-kaluramkharra.vercel.app/"
 ]
 
 
